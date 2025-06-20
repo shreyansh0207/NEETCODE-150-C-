54529a3a3cafebdbfb23169c3af5f969115a66d9
shreyansh0207 <shuklashreyansh0207@gmail.com>
2025-06-20 16:11:41 +0530

    Done

---
+++ Duplicate.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Find duplicates
+    cout << "Duplicate elements are: ";
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] == arr[j]) {
+                cout << arr[i] << " ";
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
