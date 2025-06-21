e333b8018cc978962a84866c5aee70d8df291de5
shreyansh0207 <shuklashreyansh0207@gmail.com>
2025-06-21 16:08:36 +0530

    ValidAnagram

---
+++ ValidAnagram.cpp
@@ -0,0 +1,46 @@
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         if(s.length() != t.length()) return false;
+//         int countS[26] = {0};
+//         int countT[26] = {0};
+
+//         for (int i = 0; i < s.length(); i++) {
+//             countS[s[i] - 'a']++; 
+//             countT[t[i] - 'a']++; 
+//         }
+
+//         for(int i =0; i<26; i++){
+//             if(countS[i] != countT[i]){
+//                 return false;
+//             }
+//         }
+//         return true;
+
+//     }
+// };
+
+#include <iostream>
+#include <unordered_map>
+#include <string>   
+using namespace std;
+int main(){
+    string s ; string t;
+    cout<<"Enter two strings: ";
+    cin>>s>>t;
+    if(s.length() != t.length()){
+        return false;
+    }
+    int countS[26] = {0};
+    int countT[26] = {0};
+    for(int i=0;i<s.length();i++){
+        countS[s[i]-'a']++;
+        countT[t[i]-'a']++;
+    }
+    for(int i=0;i<26;i++){
+        if(countS[i] != countT[i]){
+            return false;
+        }
+    }
+    return true;
+}
