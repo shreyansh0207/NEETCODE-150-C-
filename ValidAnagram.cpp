c16343ac1951a3d6a3070b66e1e7b453d7526977
shreyansh0207 <shuklashreyansh0207@gmail.com>
2025-06-21 16:13:34 +0530

    ValidAnagram

--- ValidAnagram.cpp
+++ ValidAnagram.cpp
@@ -1,3 +1,10 @@
+//IT MEANS IF s = "anagram" and t = "nagaram", then return true.{in this we can see that all the letter that are present in s are
+// also present in t and vice versa.}
+// If s = "rat" and t = "car", then return false. 
+
+
+
+
 // class Solution {
 // public:
 //     bool isAnagram(string s, string t) {
