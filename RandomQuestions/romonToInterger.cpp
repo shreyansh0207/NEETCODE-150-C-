5d90a48a220eda4b262121191deec36eefe03766
shreyansh0207 <shuklashreyansh0207@gmail.com>
2025-06-25 18:32:12 +0530

    RandomQuestion romanToInt

--- RandomQuestions/romonToInterger.cpp
+++ RandomQuestions/romonToInterger.cpp
@@ -10,14 +10,12 @@ public:
             {'D', 500},
             {'M', 1000}
         };
-
         int total = 0;
-
         for (int i = 0; i < s.length(); i++) {
-            // Get the value of current symbol
+            
             int current = romanValue[s[i]];
 
-            // If next symbol exists and is greater, subtract current
+           
             if (i + 1 < s.length() && romanValue[s[i]] < romanValue[s[i + 1]]) {
                 total -= current;
             } else {
