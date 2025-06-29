4717d36a58a3df81a102908560d318d3d420df96
shreyansh0207 <shuklashreyansh0207@gmail.com>
2025-06-29 23:58:08 +0530

    RandomQuestion numSubseq

---
+++ RandomQuestions/numSubseq.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int numSubseq(vector<int>& nums, int target) {
+       int mod = 1e9 + 7;
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+
+        // Precompute powers of 2 up to n
+        vector<int> pow2(n + 1, 1);
+        for (int i = 1; i <= n; ++i) {
+            pow2[i] = (pow2[i - 1] * 2) % mod;
+        }
+
+        int left = 0, right = n - 1;
+        int result = 0;
+
+        while (left <= right) {
+            if (nums[left] + nums[right] <= target) {
+                result = (result + pow2[right - left]) % mod;
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return result;
+
+    }
+};
