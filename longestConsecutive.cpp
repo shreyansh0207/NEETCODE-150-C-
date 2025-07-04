95915182a5ba65e829835167446bf2c56a0cb86b
shreyansh0207 <shuklashreyansh0207@gmail.com>
2025-07-04 22:55:19 +0530

    longestConsecutive

--- longestConsecutive.cpp
+++ longestConsecutive.cpp
@@ -24,3 +24,30 @@ public:
     }
 
 };
+
+// class Solution {
+// public:
+//     int longestConsecutive(vector<int>& nums) {
+//         unordered_set<int> numSet(nums.begin(), nums.end());
+//         int longest = 0;
+
+//         for (int num : numSet) {
+//             // Only start if it's the beginning of a sequence
+//             if (numSet.find(num - 1) == numSet.end()) {
+//                 int currentNum = num;
+//                 int count = 1;
+
+//                 // Count up the sequence
+//                 while (numSet.find(currentNum + 1) != numSet.end()) {
+//                     currentNum++;
+//                     count++;
+//                 }
+
+//                 longest = max(longest, count);
+//             }
+//         }
+
+//         return longest;
+//     }
+
+// };
