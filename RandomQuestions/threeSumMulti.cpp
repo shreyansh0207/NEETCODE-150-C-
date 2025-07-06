4292bfb37ea72bd64ea5b7845a7e2e5b4c292e5f
shreyansh0207 <shuklashreyansh0207@gmail.com>
2025-07-06 17:39:16 +0530

    RandomQuestion threeSumMulti

--- RandomQuestions/threeSumMulti.cpp
+++ RandomQuestions/threeSumMulti.cpp
@@ -19,3 +19,37 @@ public:
         return (int)count;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     int threeSumMulti(vector<int>& arr, int target) {
+//         const int MOD = 1e9 + 7;
+//         long long ans = 0;
+//         vector<long long> freq(101, 0); 
+
+//         for (int num : arr) {
+//             freq[num]++;
+//         }
+
+//         for (int i = 0; i <= 100; ++i) {
+//             for (int j = i; j <= 100; ++j) {
+//                 int k = target - i - j;
+//                 if (k < 0 || k > 100) continue;
+
+               
+//                 if (i == j && j == k) {
+//                     ans += (freq[i] * (freq[i] - 1) * (freq[i] - 2)) / 6;
+//                 } else if (i == j && j != k) {
+//                     ans += (freq[i] * (freq[i] - 1) / 2) * freq[k];
+//                 } else if (i < j && j < k) {
+//                     ans += freq[i] * freq[j] * freq[k];
+//                 }
+              
+//             }
+//         }
+
+//         return ans % MOD;
+//     }
+// };
