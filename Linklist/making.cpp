f40e1c388dab653fdc41156e04f20fc291924362
shreyansh0207 <shuklashreyansh0207@gmail.com>
2025-07-08 16:09:46 +0530

    Making of LinkList

---
+++ Linklist/making.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public :
+    int data;
+    Node* next;
+    Node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+
+int main(){
+
+    Node* head;
+    head = new Node(10);
+    cout<<head->data<<endl;
+    cout<<head->next<<endl;
+    return 0;
+}
