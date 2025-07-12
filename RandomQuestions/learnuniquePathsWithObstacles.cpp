e9fcb65a1229fddeaed5dc1775c17da99ceb7127
shreyansh0207 <shuklashreyansh0207@gmail.com>
2025-07-12 14:37:28 +0530

    RandomQuestion uniquePathsWithObstacles

--- RandomQuestions/learnuniquePathsWithObstacles.cpp
+++ RandomQuestions/learnuniquePathsWithObstacles.cpp
@@ -4,11 +4,17 @@ public:
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
 
+
+
         if (obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1) return 0;
 
 
 
         vector<vector<int>> dp(m, vector<int>(n, 0));
+
+
+
+        
 
         dp[0][0] = 1;
 
