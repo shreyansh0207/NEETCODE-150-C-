595fff62896799208258b17a11dcb86735ce73bb
shreyansh0207 <shuklashreyansh0207@gmail.com>
2025-07-16 22:31:30 +0530

    RandomQuestion myAtoi

--- RandomQuestions/myAtoi.cpp
+++ RandomQuestions/myAtoi.cpp
@@ -16,8 +16,6 @@ public:
         long result = 0;
         while (i < n && isdigit(s[i])) {
             result = result * 10 + (s[i] - '0');
-
-            
             if (sign == 1 && result > INT_MAX) return INT_MAX;
             if (sign == -1 && -result < INT_MIN) return INT_MIN;
 
