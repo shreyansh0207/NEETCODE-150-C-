82cc225089e275542710a82529b8de59ce5aaee7
shreyansh0207 <shuklashreyansh0207@gmail.com>
2025-07-19 19:46:25 +0530

    RandomQuestion lengthOfLongestSubstring

---
+++ RandomQuestions/lengthOfLongestSubstring.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int (string s) {
+        unordered_set<char> seen;
+        int left = 0, right = 0;
+        int maxLength = 0;
+
+        while (right < s.length()) {
+            if (seen.find(s[right]) == seen.end()) {
+                
+                seen.insert(s[right]);
+                maxLength = max(maxLength, right - left + 1);
+                right++;
+            } else {
+                
+                seen.erase(s[left]);
+                left++;
+            }
+        }
+
+        return maxLength;
+    }
+};
